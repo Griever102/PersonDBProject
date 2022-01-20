123426f2be5cc13621d4a84751e8fe90d3cfebce
Justin Hart <jhart91990@gmail.com>
2022-01-20 13:15:45 -0700

    Yes

---
+++ PersonInfoDB/dbo/Stored Procedures/spPeople_GetInfo.sql
@@ -0,0 +1,12 @@
+﻿create PROCEDURE [dbo].[spPeople_GetInfo]
+	@LastName nvarchar(50)
+as
+begin
+	SET NOCOUNT ON;
+
+	select FirstName, LastName, PhoneNumber
+	from dbo.Person
+	inner join dbo.Data
+	on dbo.Person.Id = dbo.Data.PersonID
+	where LastName = @LastName;
+end
